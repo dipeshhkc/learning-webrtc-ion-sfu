3b8375f02cf0dca7e467f1261a17a2d9619be84c
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-06-05 16:34:36 +0545

    fix: css styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [receiverEnabled, setReceiverEnabled] = useState(false);
 
   return (
-    <div className="bg-gray-800 min-h-screen">
+    <div className="bg-gray-800 min-h-screen overflow-auto">
       <div className="bg-gray-500 w-72 h-72 rounded-full fixed right-10 bottom-10 overflow-hidden z-10">
         {!senderEnabled ? (
           <button
@@ -20,7 +20,7 @@ function App() {
           <Sender />
         )}
       </div>
-      <div className="relative h-128">
+      <div className="relative mt-10">
         {!receiverEnabled ? (
           <button
             className="bg-blue-700 absolute top-10 left-1/2 transform -translate-x-1/2 text-white max-h-10 max-w-60 rounded-md px-5 py-2"
