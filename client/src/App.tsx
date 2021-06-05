a0faa315c623aba473b5e38d35488f662d7015ea
dipesh <kcdipesh429@gmail.com>
2021-06-05 11:34:29 +0545

    style:add tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Sender } from './Sender';
 import { Receiver } from "./Receiver";
 import { useState } from 'react';
@@ -10,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>ION SFU CLIENT</h1>
+      <p>Video Streaming</p>
       <div>
         <button onClick={() => setSenderEnabled(e => !e)}>Enable Sender</button>
         <button onClick={() => setReceiverEnabled(e => !e)}>Enable Receiver</button>
